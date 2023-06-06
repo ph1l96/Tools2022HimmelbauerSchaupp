20c9bc956b0d8e655821cd0098855fa14b836af5
s2210237017 <s2210237017@fhooe.at>
2023-06-06 16:51:21 +0200

    Commit with new method

--- david_philipp_android/src/Main.java
+++ david_philipp_android/src/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
 
+	public void thisIsANewMethod() { 
+		System.out.println("newMethod");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("DummyProject");
